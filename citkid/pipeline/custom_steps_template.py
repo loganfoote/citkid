2c74f4adbc2f103bfe7c95c3e27a4651fa1a4a82
Logan Foote <89160069+loganfoote@users.noreply.github.com>
2026-01-05 13:12:15 -0800

    Updated pipeline.custom_steps_template

--- citkid/pipeline/custom_steps_template.py
+++ citkid/pipeline/custom_steps_template.py
@@ -1,6 +1,6 @@
-import numpy as np 
+import numpy as np
 import os
-from citkid.pipeline_dev.pl_steps import plStep
+from citkid.pipeline.framework import plStep
 
 def load_global_data(directory):
     d = os.path.join(directory, 'raw/')
@@ -16,7 +16,7 @@ def load_global_res_data(directory):
     def get_path(name):
         return os.path.join(d, f'{name}_3K.npy')
     ares = np.load(get_path('ares'))
-    fres = np.load(get_path('fres')) 
+    fres = np.load(get_path('fres'))
     qres = np.load(get_path('qres'))
     res_idxs = np.load(get_path('res_indices')).astype(int)
     return fres, qres, ares, res_idxs
@@ -28,7 +28,7 @@ def load_data_t(directory, data_idx, batches):
     d = os.path.join(directory, 'raw/')
     zt = None
     for batch in batches:
-        zi = np.load(os.path.join(d, f'noise_3K_batch{batch:02d}.npy'), 
+        zi = np.load(os.path.join(d, f'noise_3K_batch{batch:02d}.npy'),
                     mmap_mode = 'r')
         if zt is None:
             zt = zi[data_idx]
@@ -39,19 +39,19 @@ def load_data_t(directory, data_idx, batches):
 def load_data_f(directory, data_idx):
     """
     data_idx can be single or list of res indices
-    """ 
+    """
     d = os.path.join(directory, 'raw/')
     ff, i, q = np.load(os.path.join(d, 's21_fine_3K.npy'), mmap_mode = 'r')
     zf = i[data_idx] + 1j * q[data_idx]
     return ff[data_idx], zf
 
 custom_steps =\
-[('load_global_data', load_global_data, ['directory'], 
-  ['fres_all', 'qres_all', 'dt'], 
+[('load_global_data', load_global_data, ['directory'],
+  ['fres_all', 'qres_all', 'dt'],
    'global'),
- ('load_global_res_data', load_global_res_data, 
+ ('load_global_res_data', load_global_res_data,
   ['directory'], ['fres', 'qres', 'ares', 'res_idxs'], 'global-res'),
- ('load_data_t', load_data_t, 
+ ('load_data_t', load_data_t,
   ['directory', 'data_idx', 'batches'], ['zt'], 'vectorized'),
  ('load_data_f', load_data_f,
   ['directory', 'data_idx'], ['ff', 'zf'], 'vectorized')
